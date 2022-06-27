f8bdcc450019084e258e0f949f4f43d8a894cfda
DmitriPrijomosev <dmitri@dezajno.ee>
2022-06-27 20:38:36 +0300

    Routing: npm i react

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Avaleht from './pages/Avaleht';
+import Ostukorv from './pages/Ostukorv';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
+	const Avaleht = <div>Avalehe leht</div>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+		  <button className="nupp">Vajuta mind!</button>
+		  <img className="pilt" src="https://pritsiproff.ee/wp-content/uploads/2022/05/sodablasting-e1653563315450.jpg" alt="" />
+		  <link to="/avaleht">
+			  <button>Avalehele</button>
+		  </link>
+		  <Routes>
+			  {/* localhost:3000/avaleht            näitab seda HTMLi */}
+			  <Route path="avaleht" element={ <Avalehet /> } />
+			  <Route path="ostukorv" element={ <Ostukorvi leht /> } />
+		  </Routes>
+	  </div>
   );
 }
 
