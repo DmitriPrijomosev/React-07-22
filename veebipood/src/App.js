3f425bfbd052a8cfceb3ad5eef131060fd968791
DmitriPrijomosev <dmitri@dezajno.ee>
2022-06-29 18:04:00 +0300

    vahel tehtud

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import Avaleht from './pages/Avaleht';
-import Ostukorv from './pages/Ostukorv';
 import { Link, Route, Routes } from 'react-router-dom';
+import Ostukorv from './Pages/Ostukorv';
+import Avaleht from './Pages/Avaleht';
 
 function App() {
-	const Avaleht = <div>Avalehe leht</div>;
-  return (
+	 return (
     <div className="App">
-
-		  <button className="nupp">Vajuta mind!</button>
-		  <img className="pilt" src="https://pritsiproff.ee/wp-content/uploads/2022/05/sodablasting-e1653563315450.jpg" alt="" />
-		  <link to="/avaleht">
-			  <button>Avalehele</button>
-		  </link>
-		  <Routes>
-			  {/* localhost:3000/avaleht            näitab seda HTMLi */}
-			  <Route path="avaleht" element={ <Avalehet /> } />
-			  <Route path="ostukorv" element={ <Ostukorvi leht /> } />
-		  </Routes>
+		<button className="nupp">Vajuta mind!</button>
+		<img className="pilt" src="https://pritsiproff.ee/wp-content/uploads/2022/05/sodablasting-e1653563315450.jpg" alt="" />
+		<link to="/avaleht">
+			<button>Avalehele</button>
+		</link>
+		<Routes>
+			{/* localhost:3000/avaleht            näitab seda HTMLi */}
+			<Route path="avaleht" element={ <Avaleht leht /> } />
+			<Route path="ostukorv" element={ <Ostukorv leht /> } />
+		</Routes>
 	  </div>
   );
 }
